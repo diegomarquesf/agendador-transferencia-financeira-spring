4301f32c363daf3f3389c8ae6e9a90fc97733300
Diego Marques <fariia_di@hotmail.com>
2025-09-05 22:13:06 -0300

    Iniciando Aplicação

---
+++ src/main/java/br/com/teste/diegomarques/agendador_transferencia_financeira/AgendadorTransferenciaFinanceiraApplication.java
@@ -0,0 +1,13 @@
+package br.com.teste.diegomarques.agendador_transferencia_financeira;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendadorTransferenciaFinanceiraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendadorTransferenciaFinanceiraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/teste/diegomarques/agendador_transferencia_financeira/AgendadorTransferenciaFinanceiraApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.teste.diegomarques.agendador_transferencia_financeira;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendadorTransferenciaFinanceiraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
